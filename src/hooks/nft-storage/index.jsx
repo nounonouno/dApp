846c0be79c93f333be3733aee224bb9518cf40b8
cryptickoan <me@cryptickoan.com>
2022-01-15 23:27:12 -0600

    :sparkles: its fixed :sparkles:

--- src/hooks/nft-storage/index.jsx
+++ src/hooks/nft-storage/index.jsx
@@ -1,5 +1,4 @@
 import { NFTStorage, File } from 'nft.storage'
-import { pack } from 'ipfs-car/pack';
 
 export const useCreateImage = async (image) => {
     const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGZiNWJhOGI2YWIwNmMyNDNCYWRCMUJhMDY3RDFENUZEYTU0Q2E0ZDUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0MjE5NTIxNzA0MywibmFtZSI6Im5vdW5vbm91bm8ifQ.T1r0xFVICepdCBRbC3zF7SregP3GjTZLr7wNU37epQM'
